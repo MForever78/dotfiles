e848408de2d63b82caf5c285161aad81fe23ae94
MForever78 <shunjian1128@gmail.com>
2018-11-29 15:22:21 +0800

    install .vimrc to neovim config

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 find ~/dotfiles -maxdepth 1 -name '.*' ! -iname ".*.sw*" ! -path ~/dotfiles/ ! -path ~/dotfiles/.git ! -path ~/dotfiles/install.sh -exec ln -svf {} $HOME \;
 
+mkdir -p $HOME/.config/nvim
+ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
+
